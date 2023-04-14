148dc62057ba8804fe1973b1b52ec336d84ccf0a
Mattciao96 <matt.ciao@gmail.com>
2023-04-14 16:43:18 +0200

    fixes

--- R/strsplit1.R
+++ R/strsplit1.R
@@ -1,3 +1,16 @@
+#' Split a string into substrings using a specified delimiter
+#'
+#' This function splits a string into substrings using a specified delimiter.
+#'
+#' @param x A character vector containing the strings to split.
+#' @param split A character string specifying the delimiter to use for splitting.
+#'
+#' @return A character vector containing the first element of the result of splitting \code{x} by \code{split}.
+#'
+#' @examples
+#' strsplit1("hello world", " ")
+#'
+#' @export
 strsplit1 <- function(x, split) {
   strsplit(x, split = split)[[1]]
 }
